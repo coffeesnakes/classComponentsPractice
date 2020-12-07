4c1477855caf0704f07ba7f19b5abf5c2a6d4349
Jacob Berg <coffeesnakes@gmail.com>
2020-12-07 08:31:14 -0800

    refactored to class based components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React  from 'react';
+import ReactDOM from 'react-dom';
 
 
-const App = () => {
+class App extends React.Component {
+  render() {
+           window.navigator.geolocation.getCurrentPosition(
+             pos => console.log(pos),
+             err => console.log("failed to get location", err)
+           );
+           return (<div> Latitude: </div>)
+  }
+}
 
+export default App;
 
-return <div style={{ backgroundColor: "#FFFFFF" }}>
-hellofriend. </div>
-};
-
-export default App
